f81c0fb21568c0c96b1c96c03757aa899e37f931
unknown <ningjiagao2022@163.com>
2023-10-10 20:07:40 +0800

    修改12题

--- lab1/12.py
+++ lab1/12.py
@@ -1,4 +1,10 @@
 #请设计一个求三次方跟的算法（不允许直接调用求方根的函数）
-n = float(input("请输入一个数"))
-x = n ** (1/3)
-print(x)
+def cube_root(n):
+    x = n / 3  # 初始猜测值
+    while True:
+        new_x = (2 * x + n / (x ** 2)) / 3
+        if abs(new_x - x) < 1e-6: # 判断迭代是否收敛
+            return new_x
+        x = new_x
+num=int(input())
+print(cube_root(num))
